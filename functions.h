f982d30e603c8ba5d1951ca7c2cd4558edd1b7f8
PakuPaku01 <wadoud.zouag@gmail.com>
2021-04-10 20:35:31 +0100

    SDL show champion icons

--- functions.h
+++ functions.h
@@ -1,6 +1,25 @@
+#include "structs.h"
+
 void initSDL();
+
 void prepareScene();
+
 void presentScene();
+
 void doInput();
-SDL_Texture *loadTexture(char *filename);
+
+void *loadTextures(Champion *list, int nbrOfChamps);
+
+void *calculateChampionPositions(Champion *list, int nbrOfChamps);
+
 void blit(SDL_Texture *texture, int x, int y);
+
+int getUserChoice(int nbrOfChampions);
+
+Champion *loadChampionList();
+
+int getFirstChampionHorizontalOffset();
+
+int getFirstChampionVerticalOffset(int nbrOfLines);
+
+void renderChamps(Champion *list, int nbrOfChamps);
--- main.c
+++ main.c
@@ -1,14 +1,16 @@
 #include <SDL2/SDL.h>
 #include "defs.h"
 #include "functions.h"
-#include "structs.h"
 
 int main(int argc, char **argv) {
-
-
+    int nbrOfChamps = getUserChoice(nbrOfChamps);
+    Champion *listOfChamps = loadChampionList();
     initSDL();
+    loadTextures(listOfChamps, nbrOfChamps);
+    calculateChampionPositions(listOfChamps, nbrOfChamps);
     while (1) {
         prepareScene();
+        renderChamps(listOfChamps, nbrOfChamps);
         doInput();
         presentScene();
         SDL_Delay(16);
